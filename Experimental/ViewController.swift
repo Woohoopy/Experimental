f9aa73efa842521c2a14d50cf6de9f708d73999a
Woohoopy <woohoopy@aim.com>
2015-07-09 12:20:22 -0500

    Added top label.

--- Experimental/ViewController.swift
+++ Experimental/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var topLabel1: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        topLabel1.text = "Swago"
     }
 
     override func didReceiveMemoryWarning() {
